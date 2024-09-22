d1f6717b0963cef6233160ebda044baa4c2185de
Sheetal Verma <148845414+Sheetalv766@users.noreply.github.com>
2024-09-22 22:40:05 +0530

    Create App.java

---
+++ src/main/App.java
@@ -0,0 +1,12 @@
+package com.crio.QKART_TestNG;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
+
